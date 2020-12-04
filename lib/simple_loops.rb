6bf3b00473270c8dfb098e66ad1ab367034c2b1e
Chip Lempke <clempke90@gmail.com>
2020-12-04 18:55:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,12 @@ def loop_message_n_times (message, limit)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
